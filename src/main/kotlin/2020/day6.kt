f9f74c4c548aad5d7de5c8cb3683847b1522deae
bmaum321 <103905368+bmaum321@users.noreply.github.com>
2022-12-13 22:34:01 -0500

    day 6 2020

---
+++ src/main/kotlin/2020/day6.kt
@@ -0,0 +1,49 @@
+package `2020`
+
+import java.io.File
+
+fun main() {
+    val nl = System.lineSeparator()
+    val groups = File("src/main/kotlin/inputs/2020/day6.txt")
+        .readText()
+        .trim()
+        .split("$nl$nl")
+
+
+    val answer = groups.sumOf { it.replace(nl,"").toSet().count() } // I originally had .toCharArray().distinct() toSet removes a function call
+
+    println(answer)
+
+    var totalCommonAnswers = 0
+
+    /**
+     * My solution
+     */
+    groups.forEach { group ->
+        val numberOfLinesInGroup = group.split(nl).size
+        val commonAnswersInGroup = group
+            .split(nl)
+            .map { it.toList() }
+            .flatten()
+            .groupingBy { it }
+            .eachCount()
+            .filterValues { it == numberOfLinesInGroup }
+            .count()
+        totalCommonAnswers += commonAnswersInGroup
+    }
+    println(totalCommonAnswers)
+
+
+    val secondAnswer2 = groups.map { group ->
+        group.split(nl).map(String::toSet)}
+    /**
+     * Jetbrains solution
+     */
+    val secondAnswer = groups.map { group ->
+        group.split(nl).map(String::toSet)
+    }.sumOf { answerSets ->
+        answerSets.reduce { a, b -> a intersect b  }.count()
+    }
+
+    println(secondAnswer)
+}
